4728ecac8383579366a803a5cc8997f7558c09d2
Paul <paul.gareze@gmail.com>
2021-01-02 18:15:52 +0000

    Initial working version of app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import React, {Component} from 'react';
-import DisplayName from './components/displayName';
+import React, { Component } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import CurrentWeek from "./components/currentWeek";
+import Home from "./components/home";
+import AddDrink from "./components/addDrink";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+const ContentDiv = styled.div`
+  flex-grow: 1;
+  text-align: justify;
+  text-justify: auto;
+`;
 
 class App extends Component {
-    render() {
-        return (
-            <DisplayName displayName={this.state.displayName} />
-        )
-    }
+  render() {
+    return (
+      <StyledContainer>
+        <BrowserRouter>
+          <Navbar expand="sm" bg="primary" variant="dark" collapseOnSelect>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav>
+                <Nav.Item>
+                  <LinkContainer to="/home">
+                    <Nav.Link>Home</Nav.Link>
+                  </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                  <LinkContainer to="/currentWeek">
+                    <Nav.Link>Current Week</Nav.Link>
+                  </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                  <LinkContainer to="/addDrink">
+                    <Nav.Link>Log Drink</Nav.Link>
+                  </LinkContainer>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
 
-    state = {
-      displayName: []
-    };
+          <ContentDiv>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to="/home" />
+              </Route>
+              <Route exact path="/home">
+                <Home
+                  currentWeek={this.state.currentWeek}
+                  total={this.state.total}
+                />
+              </Route>
+            </Switch>
+            <Switch>
+              <Route path="/currentWeek">
+                <CurrentWeek drinks={this.state.drinks} />
+              </Route>
+              <Route path="/addDrink">
+                <AddDrink />
+              </Route>
+            </Switch>
+          </ContentDiv>
+        </BrowserRouter>
+      </StyledContainer>
+    );
+  }
 
-    componentDidMount() {
-        fetch('https://immense-lowlands-99162.herokuapp.com/api/helloEndpoint')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ displayName: data })
-            })
-            .catch(console.log)
-    }
+  state = {
+    drinks: [],
+    total: [],
+    currentWeek: [],
+  };
+
+  componentDidMount() {
+    fetch("https://immense-lowlands-99162.herokuapp.com/api/drinksThisWeek")
+      .then((res) => res.json())
+      .then((drinksData) => {
+        this.setState({ drinks: drinksData });
+      })
+      .catch(console.log);
+
+    fetch("https://immense-lowlands-99162.herokuapp.com/api/totalUnitsThisWeek")
+      .then((res) => res.json())
+      .then((totalData) => {
+        this.setState({ total: totalData });
+      })
+      .catch(console.log);
+
+    fetch("https://immense-lowlands-99162.herokuapp.com/api/currentWeek")
+      .then((res) => res.json())
+      .then((week) => {
+        this.setState({ currentWeek: week });
+      })
+      .catch(console.log);
+  }
 }
 
 export default App;
