8b608589b51ccb2baa3ace3afde4b13dda0b848b
Paul Gareze <paul.gareze@gmail.com>
2021-01-16 15:20:33 +0000

    Toggle calculate units input fields
     - Allow calculate units input fields to be toggled on or off as required.

--- src/components/addDrink.js
+++ src/components/addDrink.js
@@ -29,13 +29,30 @@ export default class AddDrink extends Component {
   toggle = () =>
     this.setState((currentState) => ({ show: !currentState.show }));
 
-  submitDrink(event) {
+  submitUnitsCalculation(event) {
+    event.preventDefault();
+
+    let drink = {
+      volume: this.volume.current.value,
+      abv: this.abv.current.value,
+    };
+
+    fetch(`${process.env.REACT_APP_BASE_URL}api/addDrink`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(drink),
+    }).then((response) => response.json());
+
+    window.location.reload();
+  }
+
+  submitUnits(event) {
     event.preventDefault();
 
     let drink = {
       units: this.textInput.current.value,
-      volume: this.volume.current.value,
-      abv: this.abv.current.value,
     };
 
     fetch(`${process.env.REACT_APP_BASE_URL}api/addDrink`, {
@@ -52,45 +69,55 @@ export default class AddDrink extends Component {
   render() {
     return (
       <ContentDiv>
-        <Form onSubmit={this.submitDrink.bind(this)}>
-          <Form.Check
-            size="lg"
-            type="switch"
-            id="custom-switch"
-            label="Units Unknown"
-            onClick={this.toggle}
-          />
-          <InputGroup className="mb-3" size="lg">
-            <InputGroup.Prepend>
-              <InputGroup.Text>Volume(ml) and %</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-              ref={this.volume}
-              type="text"
-              onClick={this.focusTextInput}
-            />
-            <FormControl
-              ref={this.abv}
-              type="text"
-              onClick={this.focusTextInput}
-            />
-          </InputGroup>
-          <InputGroup size="lg">
-            <FormControl
-              ref={this.textInput}
-              type="text"
-              onClick={this.focusTextInput}
-            />
-            <InputGroup.Append>
-              <InputGroup.Text>Units</InputGroup.Text>
-            </InputGroup.Append>
-          </InputGroup>
-          <ContentDiv>
-            <Button block type="submit" name="action">
-              Submit
-            </Button>
-          </ContentDiv>
-        </Form>
+        <Form.Check
+          size="lg"
+          type="switch"
+          id="custom-switch"
+          label="Units Unknown"
+          onClick={this.toggle}
+        />
+        {this.state.show ? (
+          <Form onSubmit={this.submitUnitsCalculation.bind(this)}>
+            <InputGroup className="mb-3" size="lg">
+              <FormControl
+                ref={this.volume}
+                type="text"
+                onClick={this.focusTextInput}
+              />
+              <FormControl
+                ref={this.abv}
+                type="text"
+                onClick={this.focusTextInput}
+              />
+              <InputGroup.Append>
+                <InputGroup.Text>Volume(ml) and %</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+            <ContentDiv>
+              <Button block type="submit" name="action">
+                Submit
+              </Button>
+            </ContentDiv>
+          </Form>
+        ) : (
+          <Form onSubmit={this.submitUnits.bind(this)}>
+            <InputGroup size="lg">
+              <FormControl
+                ref={this.textInput}
+                type="text"
+                onClick={this.focusTextInput}
+              />
+              <InputGroup.Append>
+                <InputGroup.Text>Units</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+            <ContentDiv>
+              <Button block type="submit" name="action">
+                Submit
+              </Button>
+            </ContentDiv>
+          </Form>
+        )}
       </ContentDiv>
     );
   }
